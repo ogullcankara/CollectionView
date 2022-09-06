1843744d8726e8cb53aa126acc040aaa24185674
ogullcankara <ogullcankara@gmail.com>
2022-09-06 23:54:05 +0300

    First Commit

---
+++ CollectionView/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  CollectionView
+//
+//  Created by Ogulcan Kara on 6.09.2022.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var hucreLabel: UILabel!
+    
+}
